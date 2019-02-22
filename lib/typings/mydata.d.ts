3aa723af450dbc6b4a9bb5502935458df5752137
Simon Lundell <simon.lundell@iteam.se>
2019-02-22 15:39:00 +0100

    chore: improve mydata typings

--- lib/typings/mydata.d.ts
+++ lib/typings/mydata.d.ts
@@ -1,1 +1,19 @@
-declare module '@mydata/client'
+declare module '@mydata/client' {
+  interface Options {
+    clientId: string
+    clientKeys: ClientKeys
+    description: string
+    displayName: string
+    eventsPath: string
+    jwksPath: string
+    keyValueStore: any
+    operator: string
+  }
+
+  interface ClientKeys {
+    privateKey: string
+    publicKey: string
+  }
+
+  function create(options: Options): any
+}
