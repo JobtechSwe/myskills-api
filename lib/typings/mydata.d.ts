4fd85a065d4b7adc9626f38949b77a6d20d37e6c
Rickard Laurin <rickard.laurin@gmail.com>
2019-02-25 14:44:42 +0100

    chore: type key value store

--- lib/typings/mydata.d.ts
+++ lib/typings/mydata.d.ts
@@ -3,6 +3,12 @@ declare module '@mydata/client' {
     [key: string]: any
   }
 
+  interface KeyValueStore {
+    load: (key: string) => Promise<string | null>
+    remove: (key: string) => Promise<number>
+    save: (key: string, value: string, ttl: number) => Promise<void>
+  }
+
   interface Options {
     clientId: string
     clientKeys: ClientKeys
@@ -10,7 +16,7 @@ declare module '@mydata/client' {
     displayName: string
     eventsPath: string
     jwksPath: string
-    keyValueStore: any
+    keyValueStore: KeyValueStore
     operator: string
   }
 
