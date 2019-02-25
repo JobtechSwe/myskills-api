c1d06dd741e5ec222dd8578c715eb2a3ed04cc76
Simon Lundell <simon.lundell@iteam.se>
2019-02-25 11:44:46 +0100

    chore: update commands

--- lib/graphql/resolvers/mutations/login.ts
+++ lib/graphql/resolvers/mutations/login.ts
@@ -1,6 +1,6 @@
+import { GraphQLFieldResolver } from 'graphql'
 import { mydataOperator } from '../../../adapters/mydata'
 import { defaultRequest } from '../../../services/consents'
-import { GraphQLFieldResolver } from 'graphql'
 
 export const login: GraphQLFieldResolver<any, any, void> = async () => {
   const request = defaultRequest(3600 * 24 * 31)
