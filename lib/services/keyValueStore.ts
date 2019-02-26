36179d92d43723e2b98e9d9ac0f7b641b6a7a8d0
Rickard Laurin <rickard.laurin@gmail.com>
2019-02-26 14:04:28 +0100

    refactor: minor refactor of keyValueStore

--- lib/services/keyValueStore.ts
+++ lib/services/keyValueStore.ts
@@ -1,7 +1,8 @@
 import redis from '../adapters/redis'
 
-export async function save(key: string, value: string, ttl?: number) {
+export const save = async (key: string, value: string, ttl?: number) => {
   await redis.set(key, value)
+
   if (typeof ttl === 'number') {
     await redis.expire(key, Math.round(ttl / 1000))
   } else {
@@ -9,13 +10,8 @@ export async function save(key: string, value: string, ttl?: number) {
   }
 }
 
-export async function load(key: string) {
-  return redis.get(key)
-}
-
-export async function remove(key: string) {
-  return redis.del(key)
-}
+export const load = (key: string) => redis.get(key)
+export const remove = (key: string) => redis.del(key)
 
 export default {
   load,
