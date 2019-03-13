26d13ad2c4fdb2e593abcaa8df96ece0192c3e50
Radu Achim <5114595+raduachim@users.noreply.github.com>
2019-03-13 17:50:23 +0100

    Update install-openshift.sh

--- infrastructure/travis/install-openshift.sh
+++ infrastructure/travis/install-openshift.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-mkdir /tmp/openshift
-wget -qO- https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz | tar xvzf - -C /tmp/openshift --strip-components=1
-export PATH=$PATH:/tmp/openshift
+sudo mkdir /tmp/openshift
+sudo wget -qO- https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz | tar xvzf - -C /tmp/openshift --strip-components=1
+sudo export PATH=$PATH:/tmp/openshift
