a7fcf8ac98f1cbcff7d1b6b74086a3e517670f8a
Mikael Gråborg <mikael.graborg@iteam.se>
2019-03-14 08:50:48 +0100

    cd instead of direct path

--- infrastructure/travis/deploy.sh
+++ infrastructure/travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-/tmp/openshift/oc login $OPENSHIFT_URL -u $OPENSHIFT_USER -p $OPENSHIFT_PASS --insecure-skip-tls-verify=true
-/tmp/openshift/oc project myskills
-/tmp/openshift/oc rollout latest api-ci
+cd /tmp/openshift/
+oc login $OPENSHIFT_URL -u $OPENSHIFT_USER -p $OPENSHIFT_PASS --insecure-skip-tls-verify=true
+oc project myskills
+oc rollout latest api-ci
