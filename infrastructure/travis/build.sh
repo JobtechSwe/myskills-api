374a97dfea4bccc591e521a4879bb42a56a32bf7
Radu Achim <radu.achim@iteam.se>
2019-03-14 11:20:23 +0100

    ci: 🎡 added bit of debug logs to build script

--- infrastructure/travis/build.sh
+++ infrastructure/travis/build.sh
@@ -3,14 +3,18 @@ docker login -u $DOCKER_USER -p $DOCKER_PASS
 REPO=jobtechswe/myskills-api
 
 docker build -f Dockerfile -t $REPO .
+echo $TRAVIS_BRANCH
+echo $TRAVIS_TAG
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   TAG="latest"
+  echo "building latest"
   docker tag $REPO $REPO:$TAG
   docker push $REPO:$TAG
 fi
 
 if [[ -z "$TRAVIS_TAG" ]]; then
+  echo "building tag"
   docker tag $REPO $REPO:$TRAVIS_TAG
   docker tag $REPO $REPO:"latest-tag-release"
 
