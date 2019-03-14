2173a4d2576fe1fdc7ae263991ea2f34e60b871f
Radu Achim <radu.achim@iteam.se>
2019-03-14 11:09:34 +0100

    ci: 🎡 build docker and deploy openshift apps conditionally

--- infrastructure/travis/deploy.sh
+++ infrastructure/travis/deploy.sh
@@ -2,4 +2,12 @@
 
 oc login $OPENSHIFT_URL -u $OPENSHIFT_USER -p $OPENSHIFT_PASS --insecure-skip-tls-verify=true
 oc project myskills
-oc rollout latest api-ci
+
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+  oc rollout latest api-ci
+fi
+
+if [[ -z "$TRAVIS_TAG" ]]; then
+  oc rollout latest api-test
+fi
+
