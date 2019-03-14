e6e5cb20e0ec7c4fa46c190e5d3c909cef855759
Mikael Gråborg <mikael.graborg@iteam.se>
2019-03-14 08:31:09 +0100

    remove merge conflict

--- infrastructure/travis/deploy.sh
+++ infrastructure/travis/deploy.sh
@@ -2,8 +2,4 @@
 
 /tmp/openshift/oc login $OPENSHIFT_URL -u $OPENSHIFT_USER -p $OPENSHIFT_PASS --insecure-skip-tls-verify=true
 /tmp/openshift/oc project myskills
-<<<<<<< Updated upstream
 /tmp/openshift/oc rollout latest api-ci
-=======
-/tmp/openshift/oc rollout latest api-ci
->>>>>>> Stashed changes
