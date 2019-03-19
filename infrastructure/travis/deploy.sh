6387a897234a5d9069a618782c70558bcdd4fa4f
Mikael <intemicke@gmail.com>
2019-03-19 09:47:13 +0100

    chore: throw error from failed travis scripts (#20)

--- infrastructure/travis/deploy.sh
+++ infrastructure/travis/deploy.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+err=0
+trap 'err=1' ERR
 
 oc login $OPENSHIFT_URL -u $OPENSHIFT_USER -p $OPENSHIFT_PASS --insecure-skip-tls-verify=true
 oc project myskills
-exitcode=0
+
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   oc rollout latest api-ci
-  exitcode=$?
 fi
 
 if [[ ! -z "$TRAVIS_TAG" ]]; then
   oc rollout latest api-test
-  exitcode=$?
 fi
 
 oc logout
-exit $exitcode
+
+exit $err
