64b246e4104e1c2a1cdb3afef92a4f10be239d65
Radu Achim <5114595+raduachim@users.noreply.github.com>
2019-03-19 11:40:49 +0100

    ci: 🎡 update docs for openshift in travis (#29)
    * ci: 🎡 update docs for openshift in travis
    
    * style: 💄 revert changes to .travis.yml that something did to it
    
    * fix: 🐛 move encrypted token to correct location

--- infrastructure/travis/deploy.sh
+++ infrastructure/travis/deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 err=0
 trap 'err=1' ERR
+token=`cat ./infrastructure/travis/openshift-token`
 
-oc login $OPENSHIFT_URL -u $OPENSHIFT_USER -p $OPENSHIFT_PASS --insecure-skip-tls-verify=true
-oc project myskills
+oc login $OPENSHIFT_URL --token=$token --insecure-skip-tls-verify=true
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-  oc rollout latest api-ci
+  oc rollout latest api-ci -n myskills
 fi
 
 if [[ ! -z "$TRAVIS_TAG" ]]; then
-  oc rollout latest api-test
+  oc rollout latest api-test -n myskills
 fi
 
 oc logout
