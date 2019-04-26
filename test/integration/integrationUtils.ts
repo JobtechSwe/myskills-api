7e5c1cd5cc3677aa5ef4cef24c5193d871db9faf
Tom Ekander <ekander.tom@gmail.com>
2019-04-26 14:13:26 +0200

    chore: cleanup cl
    Co-Authored-By: Jimjardland <Jimjardland@users.noreply.github.com>

--- test/integration/integrationUtils.ts
+++ test/integration/integrationUtils.ts
@@ -39,7 +39,6 @@ export const getConsentedClient = async (
     const { url, id } = await consents.request<Consent>(request)
     await approveConsent(url)
     const { accessToken } = await getConsentRequest(id)
-    console.log('accessTOken: ', accessToken)
 
     return getClient(server, {
       context: {
